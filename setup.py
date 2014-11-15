82fb8373cf88342d6d0e35f3a98ae66d6cf3041b
Nithin Philips <nithin@nithinphilips.com>
2014-11-15 02:11:03 -0500

    Updating requirements and author info.

--- setup.py
+++ setup.py
@@ -30,12 +30,12 @@ setup(
         "argcomplete",
         "bunch",
         "suds",
-        "mechanize"
+        "requests"
     ],
     version = version,
-    description = "Python command line application bare bones template.",
+    description = "Odel uploads Data Integrator files to Tririga",
     long_description = long_descr,
-    author = "Jan-Philip Gehrcke",
-    author_email = "jgehrcke@googlemail.com",
-    url = "http://gehrcke.de/2014/02/distributing-a-python-command-line-application",
+    author = "Nithin Philips",
+    author_email = "nithin@nithinphilips.com",
+    url = "",
     )
