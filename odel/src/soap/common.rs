d8b06531fe071f53db55480d7c7fe0ece902a8d9
Nithin Philips <nithin@nithinphilips.com>
2020-06-12 11:53:15 -0400

    Remove dead code

--- odel/src/soap/common.rs
+++
@@ -1,7 +0,0 @@
-use crate::soap::validate::Validate;
-use macro_utils::*;
-use std::io::{Read, Write};
-use std::str::FromStr;
-use xsd_types::types as xs;
-use yaserde::{YaDeserialize, YaSerialize};
-
